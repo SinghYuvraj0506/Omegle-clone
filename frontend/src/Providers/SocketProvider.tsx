64c9646a80abff9983b424b5ee4301e208e67103
Yuvraj <singhyuvraj0506@gmail.com>
2024-06-09 01:44:20 +0530

    fixed bugs

--- frontend/src/Providers/SocketProvider.tsx
+++ frontend/src/Providers/SocketProvider.tsx
@@ -21,6 +21,10 @@ const SocketProvider: React.FC<ContextProviderProps> = (props) => {
   const URL = import.meta.env.VITE_SERVER_URL as string;
   const socket = io(URL, {
     autoConnect: false,
+    withCredentials:true,
+    extraHeaders: {
+      "Content-Type": "application/json"
+    }
   });
 
   const [connectionState, setConnectionState] = useState(false)
